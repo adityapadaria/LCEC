8c069ab63e9b1865d5c7bd39adbc76c1f7641743
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-18 00:13:34 +0530

    Create 003.sh

---
+++ 003.sh
@@ -0,0 +1,15 @@
+# echo "\n------------------------------------------------------------------------------------------------------" # LinuxCNC QtPyVCP
+# echo "[Step:2] Install QtPyVCP:"
+# echo "------------------------------------------------------------------------------------------------------\n"
+
+# echo "\n>> Install python3-qtpyvcp: \n "
+# sudo apt install -y python3-qtpyvcp &> /dev/null
+
+# while true; do
+#     read -p "\n >> Do you wish to continue?   " yn
+#     case $yn in
+#         [Yy]* ) clear; break;;
+#         [Nn]* ) exit;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done
