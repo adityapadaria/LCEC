21b181074f12a5d2250c56efdabbcb96e129f256
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-17 14:52:41 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -21,10 +21,10 @@ make install
 make modules_install
 depmod
 
-#------------------------------------------------------------------------------------------- 
+#------------------------------------------------------------------------------------------- LinuxCNC EtherCAT Support Layer
 
 echo 
-echo "[Step:2] Installing linuxcnc-ethercat:"
+echo "[Step:2] Install linuxcnc-ethercat:"
 
 echo " Important Note: "
 echo " Trying to install without installing “ethercat-master” package, but script will install it anyways."
@@ -34,10 +34,10 @@ echo " User needs to answer “NO” to this prompt to maintain the manually ins
 cd ~
 apt install -y linuxcnc-ethercat
 
-#-------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------- LinuxCNC EtherCAT CIA402 Motor Support Layer
 
 echo 
-echo "[Step:3] Installing hal-cia402:"
+echo "[Step:3] Install hal-cia402:"
 
 cd ~
 
@@ -48,6 +48,13 @@ cd hal-cia402
 echo ">> Install hal-cia402:"
 sudo halcompile --install cia402.comp
 
+#------------------------------------------------------------------------------------------- Set Permission for EtherCAT Ports from Startup
 
+echo 
+echo "[Step:4] Set Permission for EtherCAT Ports:"
+
+touch /etc/udev/rules.d/99-ethercat.rules
+echo "KERNEL=="EtherCAT[0-9]", MODE="0777"" > /etc/udev/rules.d/99-ethercat.rules
+udevadm control --reload-rules
 
 
