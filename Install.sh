9c476f9b8c9f37623a23d6a18cbd1617752bf8ab
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-17 14:03:59 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,26 +1,18 @@
+#------------------------------------------------------------------------------------------- EtherCAT Master
+
 echo 
 echo "[Step:1] Installing IgH EtherCAT Master:"
-echo
 
-echo 
 echo ">> Installing libtool:"
-
 apt install libtool
 
-echo 
 echo ">> Download Ethercat Master Source Code:"
-echo
-
 cd ~
 git clone https://gitlab.com/etherlab.org/ethercat
 cd ethercat
 
-echo 
 echo ">> Compile and Install Ethercat Master:"
-echo
-
 autoupdate
-
 ./bootstrap
 ./configure --sysconfdir=/etc/ --disable-eoe --disable-8139too --enable-genet
 make
@@ -29,6 +21,32 @@ make install
 make modules_install
 depmod
 
+#------------------------------------------------------------------------------------------- 
+
+echo 
+echo "[Step:2] Installing linuxcnc-ethercat:"
+
+echo " Important Note: "
+echo " Trying to install without installing “ethercat-master” package, but script will install it anyways."
+echo " At the end of this build process, script will notify they it is already installed."
+echo " User needs to answer “NO” to this prompt to maintain the manually installed version without EoE."
+
+cd ~
+apt install -y linuxcnc-ethercat
+
+#-------------------------------------------------------------------------------------------
+
+echo 
+echo "[Step:3] Installing hal-cia402:"
+
+cd ~
+
+echo ">> Download hal-cia402 Source Code:"
+git clone https://github.com/dbraun1981/hal-cia402
+cd hal-cia402
+
+echo ">> Install hal-cia402:"
+sudo halcompile --install cia402.comp
 
 
 
