a0c11656a8895336330bc4a26ccb2eecd66fde8f
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-17 17:25:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,30 @@
-#------------------------------------------------------------------------------------------- Update Repository
-
-echo "--------------------------------------------------"
-echo "[Step:0] Update APT Repository:"
-echo "--------------------------------------------------"
-echo 
+echo "\n--------------------------------------------------------------------------" # Update Repository
+echo "[Step:1] Update APT Repository:"
+echo "--------------------------------------------------------------------------\n"
 
 sudo apt update
 
 # Removing old repository if exists
 if [ -e /etc/apt/sources.list.d/ighvh.sources ]
 then
-    echo ">> Remove /etc/apt/sources.list.d/ighvh.sources \n "
+    echo "\n>> Remove /etc/apt/sources.list.d/ighvh.sources \n "
     sudo rm /etc/apt/sources.list.d/ighvh.sources
 fi
 
 if [ -e /etc/apt/sources.list.d/kcjengr.list ]
 then
-    echo ">> Remove /etc/apt/sources.list.d/kcjengr.list \n "
+    echo "\n>> Remove /etc/apt/sources.list.d/kcjengr.list \n "
     sudo rm /etc/apt/sources.list.d/kcjengr.list
 fi
 
 if [ -e /etc/apt/trusted.gpg.d/kcjengr.gpg ]
 then
-    echo ">> Remove /etc/apt/trusted.gpg.d/kcjengr.gpg \n "
+    echo "\n>> Remove /etc/apt/trusted.gpg.d/kcjengr.gpg \n "
     sudo rm /etc/apt/trusted.gpg.d/kcjengr.gpg
 fi
 
 # Install updated repository
-echo ">> Install EtherCAT Repository \n "
+echo "\n>> Install EtherCAT Repository \n "
 mkdir -p /usr/local/share/keyrings/
 wget -O- https://build.opensuse.org/projects/science:EtherLab/signing_keys/download?kind=gpg | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/science_EtherLab.gpg
 tee -a /etc/apt/sources.list.d/ighvh.sources > /dev/null <<EOT
@@ -37,33 +34,36 @@ Suites: ./
 URIs: http://download.opensuse.org/repositories/science:/EtherLab/Debian_12/
 EOT
 
-echo ">> Installing curl: \n"
+echo "\n>> Installing curl: \n"
 sudo apt install -y curl
 
 # Install updated repository
-echo ">> Install QtPyVCP Repository \n "
+echo "\n>> Install QtPyVCP Repository \n "
 echo 'deb [arch=arm64] https://repository.qtpyvcp.com/apt stable main' | sudo tee /etc/apt/sources.list.d/kcjengr.list
 curl -sS https://repository.qtpyvcp.com/repo/kcjengr.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/kcjengr.gpg
 gpg --keyserver keys.openpgp.org --recv-key 2DEC041F290DF85A
 
 sudo apt update
 
-#------------------------------------------------------------------------------------------- EtherCAT Master
+echo "\n--------------------------------------------------------------------------" # LinuxCNC QtPyVCP
+echo "[Step:2] Install QtPyVCP:"
+echo "--------------------------------------------------------------------------\n"
 
-echo "--------------------------------------------------"
-echo "[Step:1] Installing IgH EtherCAT Master:"
-echo "--------------------------------------------------"
-echo 
+sudo apt install -y python3-qtpyvcp
 
-echo ">> Installing libtool: \n"
+echo "\n--------------------------------------------------------------------------" # EtherCAT Master
+echo "[Step:3] Installing IgH EtherCAT Master:"
+echo "--------------------------------------------------------------------------\n"
+
+echo "\n>> Installing libtool: \n"
 sudo apt install -y libtool
 
-echo ">> Download Ethercat Master Source Code:"
+echo "\n>> Download Ethercat Master Source Code:"
 cd ~
 git clone https://gitlab.com/etherlab.org/ethercat
-cd ethercat
 
-echo ">> Compile and Install Ethercat Master:"
+echo "\n>> Compile and Install Ethercat Master:"
+cd ethercat
 sudo autoupdate
 sudo ./bootstrap
 sudo ./configure --sysconfdir=/etc/ --disable-eoe --disable-8139too --enable-genet
@@ -73,74 +73,45 @@ sudo make install
 sudo make modules_install
 sudo depmod
 
-#------------------------------------------------------------------------------------------- LinuxCNC EtherCAT Support Layer
-
-echo "--------------------------------------------------"
-echo "[Step:2] Install linuxcnc-ethercat:"
-echo "--------------------------------------------------"
-echo 
-
-echo " Important Note: "
-echo " Trying to install without installing “ethercat-master” package, but script will install it anyways."
-echo " At the end of this build process, script will notify they it is already installed."
-echo " User needs to answer “NO” to this prompt to maintain the manually installed version without EoE."
-echo
+echo "\n--------------------------------------------------------------------------" # LinuxCNC EtherCAT Support Layer
+echo "[Step:4] Install linuxcnc-ethercat:"
+echo "Important Note: "
+echo "Trying to install without installing “ethercat-master” package, but script will install it anyways. "
+echo "At the end of this build process, script will notify they it is already installed. "
+echo "User needs to answer “NO” to this prompt to maintain the manually installed version without EoE. "
+echo "--------------------------------------------------------------------------\n"
 
 sudo apt install -y linuxcnc-ethercat
 
-#------------------------------------------------------------------------------------------- LinuxCNC EtherCAT CIA402 Motor Support Layer
+echo "\n--------------------------------------------------------------------------" # LinuxCNC EtherCAT CIA402 Motor Support Layer
+echo "[Step:5] Install hal-cia402:"
+echo "--------------------------------------------------------------------------\n"
 
-echo "--------------------------------------------------"
-echo "[Step:3] Install hal-cia402:"
-echo "--------------------------------------------------"
-echo 
-
+echo "\n>> Download hal-cia402 Source Code:"
 cd ~
-
-echo ">> Download hal-cia402 Source Code:"
 git clone https://github.com/dbraun1981/hal-cia402
-cd hal-cia402
 
-echo ">> Install hal-cia402:"
+echo "\n>> Install hal-cia402:"
+cd hal-cia402
 sudo halcompile --install cia402.comp
 
-#------------------------------------------------------------------------------------------- Set Permission for EtherCAT Ports from Startup
-
-echo "--------------------------------------------------"
-echo "[Step:4] Set Permission for EtherCAT Ports:"
-echo "--------------------------------------------------"
-echo 
+echo "\n--------------------------------------------------------------------------" # Set Permission for EtherCAT Ports from Startup
+echo "[Step:6] Set Permission for EtherCAT Ports:"
+echo "--------------------------------------------------------------------------\n"
 
 touch /etc/udev/rules.d/99-ethercat.rules
 echo "KERNEL=="EtherCAT[0-9]", MODE="0777"" > /etc/udev/rules.d/99-ethercat.rules
 udevadm control --reload-rules
 
-#------------------------------------------------------------------------------------------- LinuxCNC QtPyVCP
-
-echo "--------------------------------------------------" 
-echo "[Step:4] Install QtPyVCP:"
-echo "--------------------------------------------------"
-echo 
-
-sudo apt install -y python3-qtpyvcp
-
-#------------------------------------------------------------------------------------------- Custom Code
-
-echo "--------------------------------------------------"
-echo "[Step:5] Download Custom Code:"
-echo "--------------------------------------------------"
-echo 
-
-cd ~
-
-mkdir linuxcnc
-mkdir ./linuxcnc/configs
-cd ./linuxcnc/configs
+echo "\n--------------------------------------------------------------------------" # Custom Code
+echo "[Step:7] Download Custom Code:"
+echo "--------------------------------------------------------------------------\n"
 
 echo ">> Download hal-cia402 Source Code:"
-git clone https://github.com/adityapadaria/lcec-project
+cd ~
+git clone https://github.com/adityapadaria/lcec-project linuxcnc
 
-#-------------------------------------------------------------------------------------------
+echo "\n--------------------------------------------------------------------------" 
 
 sudo /etc/init.d/ethercat start
 sudo chmod 666 /dev/EtherCAT0
