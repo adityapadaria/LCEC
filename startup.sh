e93c08c60e8df50e8f50d588048f703a29f5f667
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-17 21:07:01 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,12 @@
-sudo su
-MAC_ADDR=$(ethtool -P eth0 | awk ‘{print $NF}’)
+#! /bin/bash
+
+# MAC_ADDR=$(sudo ethtool -P eth0 | awk ‘{print $NF}’)
+
+MAC_ADDR=$(ip link show eth0 | awk '/ether/ {print $2}')
 modprobe ec_master main_devices=MAC_ADDR
 modprobe ec_genet
 echo fd580000.ethernet > /sys/bus/platform/drivers/bcmgenet/unbind
 echo fd580000.ethernet > /sys/bus/platform/drivers/ec_bcmgenet/bind
 chmod 666 /dev/EtherCAT0
+
+exit 0
