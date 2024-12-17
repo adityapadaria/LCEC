06912b1f4c09b48db8a7ed13b8d2ad3c0622c7ba
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-18 00:12:46 +0530

    Update 002.sh

--- 002.sh
+++ 002.sh
@@ -1,29 +1,45 @@
 #! /bin/bash
 
-echo "\n------------------------------------------------------------------------------------------------------" # Update Repository
+echo " "
+echo "------------------------------------------------------------------------------------------------------" # Update Repository
 echo "[Step:2] Update APT Repository:"
-echo "------------------------------------------------------------------------------------------------------\n"
+echo "------------------------------------------------------------------------------------------------------"
+echo " "
 
 if [ -e /etc/apt/sources.list.d/kcjengr.list ]
 then
-    echo "\n>> Remove /etc/apt/sources.list.d/kcjengr.list \n "
+    echo " "
+    echo ">> Remove /etc/apt/sources.list.d/kcjengr.list "
+    echo " "
+    
     sudo rm /etc/apt/sources.list.d/kcjengr.list
 fi
 
 if [ -e /etc/apt/trusted.gpg.d/kcjengr.gpg ]
 then
-    echo "\n>> Remove /etc/apt/trusted.gpg.d/kcjengr.gpg \n "
+    echo " "
+    echo ">> Remove /etc/apt/trusted.gpg.d/kcjengr.gpg "
+    echo " "
+    
     sudo rm /etc/apt/trusted.gpg.d/kcjengr.gpg
 fi
 
-echo "\n>> Install curl: \n "
+echo " "
+echo ">> Install curl: "
+echo " "
 sudo apt install -y curl
 
 # Install updated repository
-echo "\n>> Install QtPyVCP Repository \n "
+echo " "
+echo ">> Install QtPyVCP Repository "
+echo " "
+
 echo 'deb [arch=arm64] https://repository.qtpyvcp.com/apt stable main' | sudo tee /etc/apt/sources.list.d/kcjengr.list
 curl -sS https://repository.qtpyvcp.com/repo/kcjengr.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/kcjengr.gpg
 gpg --keyserver keys.openpgp.org --recv-key 2DEC041F290DF85A
 
-echo "\n>> Verify Repository: \n"
+echo " "
+echo ">> Verify Repository: "
+echo " "
+
 sudo apt update
