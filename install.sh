1297b44f36599ad68ce72b46d9073bea62276c65
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-17 19:05:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,10 +65,8 @@ echo "\n>> Download Ethercat Master Source Code: \n "
 cd ~
 git clone https://gitlab.com/etherlab.org/ethercat
 
-sudo su
-
 echo "\n>> Compile and Install Ethercat Master: \n "
-sudo cd /root/ethercat
+cd /root/ethercat
 sudo autoupdate
 
 echo "\n>> Bootstrap: \n "
