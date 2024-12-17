821179658b9305a1815a93301d46508df986202e
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-18 00:05:33 +0530

    Update 001.sh

--- 001.sh
+++ 001.sh
@@ -1,18 +1,25 @@
 #! /bin/bash
 
-echo "\n------------------------------------------------------------------------------------------------------" # Update Repository
+echo "------------------------------------------------------------------------------------------------------" # Update Repository
 echo "[Step:1] Update APT Repository:"
-echo "------------------------------------------------------------------------------------------------------\n"
+echo "------------------------------------------------------------------------------------------------------"
+echo " "
 
 # Removing old repository if exists
 if [ -e /etc/apt/sources.list.d/ighvh.sources ]
 then
-    echo "\n>> Remove /etc/apt/sources.list.d/ighvh.sources \n "
+    echo " "
+    echo ">> Remove /etc/apt/sources.list.d/ighvh.sources "
+    echo " "
+    
     sudo rm /etc/apt/sources.list.d/ighvh.sources
 fi
 
 # Install updated repository
-echo "\n>> Install EtherCAT Repository \n "
+echo " "
+echo ">> Install EtherCAT Repository "
+echo " "
+
 mkdir -p /usr/local/share/keyrings/
 wget -O- https://build.opensuse.org/projects/science:EtherLab/signing_keys/download?kind=gpg | gpg --dearmor | dd of=/etc/apt/trusted.gpg.d/science_EtherLab.gpg
 tee -a /etc/apt/sources.list.d/ighvh.sources > /dev/null <<EOT
@@ -22,5 +29,8 @@ Suites: ./
 URIs: http://download.opensuse.org/repositories/science:/EtherLab/Debian_12/
 EOT
 
-echo "\n>> Verify Repository: \n"
+echo " "
+echo ">> Verify Repository: "
+echo " "
+
 sudo apt update
