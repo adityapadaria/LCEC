cfee3e23c42831bfd4f327f20e466634732e2a5e
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-18 02:44:04 +0530

    Update 04-lcec.sh

--- 04-lcec.sh
+++ 04-lcec.sh
@@ -3,16 +3,33 @@
 echo " "
 echo "------------------------------------------------------------------------------------------------------" # LinuxCNC EtherCAT Support Layer
 echo "[Step:4] Install linuxcnc-ethercat:"
-echo " "
-echo "Important Note: "
-echo "Trying to install without installing “ethercat-master” package, but script will install it anyways. "
-echo "At the end of this build process, script will notify they it is already installed. "
-echo "User needs to answer “NO” to this prompt to maintain the manually installed version without EoE. "
 echo "------------------------------------------------------------------------------------------------------"
 echo " "
 
 echo " "
-echo ">> Install linuxcnc-ethercat: "
+echo ">> Installing libethercat-dev: "
 echo " "
 
-sudo apt install -y linuxcnc-ethercat
+sudo apt install -y libethercat-dev
+
+echo " "
+echo ">> Download linuxcnc-ethercat: "
+echo " "
+
+cd ~
+git clone https://github.com/linuxcnc-ethercat/linuxcnc-ethercat
+
+echo " "
+echo ">> Compile Library: "
+echo " "
+
+cd /root/linuxcnc-ethercat
+sudo make build
+
+echo " "
+echo ">> Install Library: "
+echo " "
+
+sudo make install
+
+# sudo apt install -y linuxcnc-ethercat
