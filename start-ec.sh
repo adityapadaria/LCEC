a0cd67d38f16220c8f8cad71b8a824822c3956a3
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-19 15:15:20 +0530

    Update and rename startup.sh to start-ec.sh

---
+++ start-ec.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+if lsmod | grep -wq ec_genet; then
+  rmmod ec_genet
+fi
+
+if lsmod | grep -wq ec_master; then
+#  rmmod ec_master
+  /etc/init.d/ethercat stop
+fi
+
+modprobe ec_master main_devices=e4:5f:01:82:96:36
+modprobe ec_genet
+
+echo fd580000.ethernet > /sys/bus/platform/drivers/bcmgenet/unbind
+echo fd580000.ethernet > /sys/bus/platform/drivers/ec_bcmgenet/bind
+
+sudo chmod 666 /dev/EtherCAT0
+
+# /usr/bin/linuxcnc '/home/cnc/linuxcnc/configs/ethercatqt/ethercatqt.ini'
