5c02fc1c1f5ae013a1280e76f503efef2f08719e
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-19 15:08:01 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,22 +1,16 @@
 #! /bin/bash
 
-# git clone https://github.com/adityapadaria/lcec-install /home/cnc/scripts
-
-# cd /home/cnc/scripts
-
-# sudo chmod +x startup.sh
-
 if lsmod | grep -wq ec_genet; then
-  rmmod ec_genet
+  sudo rmmod ec_genet
 fi
 
 if lsmod | grep -wq ec_master; then
-#  rmmod ec_master
-  /etc/init.d/ethercat stop
+#  sudo rmmod ec_master
+  sudo /etc/init.d/ethercat stop
 fi
 
-modprobe ec_master main_devices=e4:5f:01:82:96:36
-modprobe ec_genet
+sudo modprobe ec_master main_devices=e4:5f:01:82:96:36
+sudo modprobe ec_genet
 
 echo fd580000.ethernet > /sys/bus/platform/drivers/bcmgenet/unbind
 echo fd580000.ethernet > /sys/bus/platform/drivers/ec_bcmgenet/bind
