1034a5cc168b0853a3cd490e74e6fa61cd378c2c
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-19 20:02:32 +0530

    Update 07x-source.sh

--- 07x-source.sh
+++ 07x-source.sh
@@ -14,6 +14,23 @@ sudo rm -r /home/cnc/linuxcnc
 
 git clone https://github.com/adityapadaria/lcec-project /home/cnc/linuxcnc
 
+sudo systemctl stop start-ec.service
+sudo systemctl stop start-lc.service
+
+sudo rm -r /etc/systemd/system/start-ec.service
+sudo rm -r /etc/systemd/system/start-lc.service
+
+sudo cp /home/cnc/linuxcnc/scripts/start-ec.service /etc/systemd/system/
+sudo cp /home/cnc/linuxcnc/scripts/start-lc.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+
+sudo systemctl enable start-ec.service
+sudo systemctl enable start-lc.service
+
+sudo systemctl start start-ec.service
+sudo systemctl start start-lc.service
+
 # sudo apt purge firefox-esr*
 # sudo apt purge golang
 # sudo apt autoremove
