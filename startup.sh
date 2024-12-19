80127c5c5923378b014654f85831761baed0f557
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-19 15:10:39 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,8 +12,8 @@ fi
 sudo modprobe ec_master main_devices=e4:5f:01:82:96:36
 sudo modprobe ec_genet
 
-echo fd580000.ethernet > /sys/bus/platform/drivers/bcmgenet/unbind
-echo fd580000.ethernet > /sys/bus/platform/drivers/ec_bcmgenet/bind
+echo fd580000.ethernet > sudo /sys/bus/platform/drivers/bcmgenet/unbind
+echo fd580000.ethernet > sudo /sys/bus/platform/drivers/ec_bcmgenet/bind
 
 sudo chmod 666 /dev/EtherCAT0
 
