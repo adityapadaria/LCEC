d2b021c5bb9774894cd943228cd83bd6d9c53231
Aditya <8377991+adityapadaria@users.noreply.github.com>
2024-12-19 17:43:53 +0530

    Update 07x-source.sh

--- 07x-source.sh
+++ 07x-source.sh
@@ -13,3 +13,10 @@ echo " "
 sudo rm -r /home/cnc/linuxcnc
 
 git clone https://github.com/adityapadaria/lcec-project /home/cnc/linuxcnc
+
+# sudo apt purge firefox-esr*
+# sudo apt purge golang
+# sudo apt autoremove
+# sudo apt autoclean
+
+# sudo systemctl disable NetworkManager-wait-online.service
